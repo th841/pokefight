a94cba8bd837cf7ec75395b7e7a72654eb7dce93
Tibor Herczeg <th@gerhardt.io>
2025-11-28 23:03:30 +0100

    Add pokefight-api and pokefight-app modules

---
+++ pokefight-app/src/test/java/org/th/pokefight/PokefightBeApplicationTests.java
@@ -0,0 +1,13 @@
+package org.th.pokefight;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PokefightBeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
