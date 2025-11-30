0ff34332f07fc72e77bc7cbd44995cb29b477edc
Tibor Herczeg <th@gerhardt.io>
2025-11-30 18:28:01 +0100

    Enable CORS for development

--- pokefight-api/src/main/java/org/th/pokefight/api/PokemonWebService.java
+++ pokefight-api/src/main/java/org/th/pokefight/api/PokemonWebService.java
@@ -2,18 +2,19 @@ package org.th.pokefight.api;
 
 import java.util.List;
 
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.th.pokefight.api.dto.PokemonDTO;
 
+@CrossOrigin(origins = "http://localhost:4200")
 @RequestMapping(PokemonWebService.PATH_POKEMONS)
 @ResponseBody
 public interface PokemonWebService {
 
     static final String PATH_POKEMONS = "/pokemons";
     static final String PATH_RANDOM = "/random";
-
 
     @GetMapping(PATH_RANDOM)
     List<PokemonDTO> getRandomPokemons();
